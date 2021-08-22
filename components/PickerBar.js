dfd1aabbd2f7455760c59d56bc110545aa08c35e
gitit4321 <gitit4321@gmail.com>
2021-08-22 12:54:13 -0400

    Added sharps/flats radio btn below three pickers

--- components/PickerBar.js
+++ components/PickerBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Picker } from 'react-native';
+import { StyleSheet, View, Picker, Image } from 'react-native';
 
 const PickerBar = props => {
     const {
@@ -12,57 +12,94 @@ const PickerBar = props => {
     } = props;
 
     return (
-        <View style={[styles.horizontalContainer, styles.pickerContainer]}>
-            <Picker
-                selectedValue={selectedKey}
-                style={{ height: 50, width: 100 }}
-                itemStyle={{
-                    height: 50,
-                    width: 100,
-                }}
-                onValueChange={(itemValue, itemIndex) =>
-                    handleKeyChange(itemValue)
-                }
-            >
-                <Picker.Item label="C" value="C" />
-                <Picker.Item label="C#/Db" value="C#/Db" />
-                <Picker.Item label="D" value="D" />
-                <Picker.Item label="D#/Eb" value="D#/Eb" />
-                <Picker.Item label="E" value="E" />
-                <Picker.Item label="F" value="F" />
-                <Picker.Item label="F#/Gb" value="F#/Gb" />
-                <Picker.Item label="G" value="G" />
-                <Picker.Item label="G#/Ab" value="G#/Ab" />
-                <Picker.Item label="A" value="A" />
-                <Picker.Item label="A#/Bb" value="A#/Bb" />
-                <Picker.Item label="B" value="B" />
-            </Picker>
-            <Picker
-                selectedValue={selectedChordQ}
-                style={{ height: 50, width: 100 }}
-                itemStyle={{
-                    height: 50,
-                }}
-                onValueChange={(itemValue, itemIndex) =>
-                    handleChordQChange(itemValue)
-                }
-            >
-                <Picker.Item label="maj7" value="maj7" />
-                <Picker.Item label="min7" value="min7" />
-            </Picker>
-            <Picker
-                selectedValue={selectedClef}
-                style={{ height: 50, width: 100 }}
-                itemStyle={{
-                    height: 50,
-                }}
-                onValueChange={(itemValue, itemIndex) =>
-                    handleClefChange(itemValue)
-                }
-            >
-                <Picker.Item label="Treble" value="Treble" />
-                <Picker.Item label="Bass" value="Bass" />
-            </Picker>
+        <View>
+            <View style={[styles.horizontalContainer]}>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={selectedKey}
+                        style={{ height: 50, width: 90 }}
+                        itemStyle={{
+                            height: 50,
+                        }}
+                        onValueChange={(itemValue, itemIndex) =>
+                            handleKeyChange(itemValue)
+                        }
+                    >
+                        <Picker.Item label="C" value="C" />
+                        <Picker.Item label="C#/Db" value="C#/Db" />
+                        <Picker.Item label="D" value="D" />
+                        <Picker.Item label="D#/Eb" value="D#/Eb" />
+                        <Picker.Item label="E" value="E" />
+                        <Picker.Item label="F" value="F" />
+                        <Picker.Item label="F#/Gb" value="F#/Gb" />
+                        <Picker.Item label="G" value="G" />
+                        <Picker.Item label="G#/Ab" value="G#/Ab" />
+                        <Picker.Item label="A" value="A" />
+                        <Picker.Item label="A#/Bb" value="A#/Bb" />
+                        <Picker.Item label="B" value="B" />
+                    </Picker>
+                    <View>
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_up.png')}
+                        />
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_down.png')}
+                        />
+                    </View>
+                </View>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={selectedChordQ}
+                        style={{ height: 50, width: 90 }}
+                        itemStyle={{
+                            height: 50,
+                        }}
+                        onValueChange={(itemValue, itemIndex) =>
+                            handleChordQChange(itemValue)
+                        }
+                    >
+                        <Picker.Item label="maj7" value="maj7" />
+                        <Picker.Item label="min7" value="min7" />
+                    </Picker>
+                    <View>
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_up.png')}
+                        />
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_down.png')}
+                        />
+                    </View>
+                </View>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={selectedClef}
+                        style={{ height: 50, width: 90 }}
+                        itemStyle={{
+                            height: 50,
+                        }}
+                        onValueChange={(itemValue, itemIndex) =>
+                            handleClefChange(itemValue)
+                        }
+                    >
+                        <Picker.Item label="Treble" value="Treble" />
+                        <Picker.Item label="Bass" value="Bass" />
+                    </Picker>
+                    <View>
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_up.png')}
+                        />
+                        <Image
+                            style={styles.arrows}
+                            source={require('../assets/arrow_down.png')}
+                        />
+                    </View>
+                </View>
+            </View>
         </View>
     );
 };
@@ -71,13 +108,18 @@ const styles = StyleSheet.create({
     horizontalContainer: {
         display: 'flex',
         flexDirection: 'row',
+        width: '95%',
+        justifyContent: 'space-between',
     },
     pickerContainer: {
         display: 'flex',
-        // height: '8%',
+        flexDirection: 'row',
         marginBottom: 16,
-        justifyContent: 'space-evenly',
-        backgroundColor: 'red',
+        alignItems: 'center',
+    },
+    arrows: {
+        height: 14,
+        width: 14,
     },
 });
 
